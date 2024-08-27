2b4c6d80221acf77875752dadda545c76fa8221d
nakashima-yuta23-fixer <nakashima-yuta23@fixer.cd.jp>
2024-08-27 19:47:04 +0900

    test-action

--- .github/workflows/tfcmt-with-terragrunt/tfwrapper.sh
+++ .github/workflows/tfcmt-with-terragrunt/tfwrapper.sh
@@ -6,7 +6,7 @@ command=$1
 
 base_dir=$(git rev-parse --show-toplevel) # Please fix if necessary
 target=${PWD#"$base_dir"/}
-config_path="$GITHUB_WORKSPACE/.github/workflows/files/tfcmt-with-terragrunt/.tfcmt.yaml"
+config_path="$GITHUB_WORKSPACE/.github/workflows/tfcmt-with-terragrunt/.tfcmt.yaml"
 
 if [ "$command" == "plan" ]; then
   tfcmt -var "target:${target}" --config "$config_path" plan -patch -- terraform "$@"
