7be998eb0ac969f893261550417b53e9b0103cab
nakashima-yuta23-fixer <nakashima-yuta23@fixer.cd.jp>
2024-09-09 11:46:52 +0900

    test

--- terraform/container_registry/main.tf
+++ terraform/container_registry/main.tf
@@ -1,5 +1,6 @@
 module "container_registry" {
-  source              = "git::https://github.com/nakashima-yuta23-fixer/TerraformCommonModules.git//container_registry?ref=main"
+  source = "git::https://github.com/nakashima-yuta23-fixer/TerraformCommonModules.git//container_registry?ref=main"
+
   product_name        = "nytes1"
   env                 = "dev"
   resource_group_name = var.resource_group_name
