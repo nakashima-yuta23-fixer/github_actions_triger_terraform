5a4e269e93a981ee31873fe64a99074ef75f19c7
nakashima-yuta23-fixer <129721088+nakashima-yuta23-fixer@users.noreply.github.com>
2024-09-09 11:20:55 +0900

    test (#7)
    * test
    
    * test
    
    * test
    
    ---------
    
    Co-authored-by: nakashima-yuta23-fixer <nakashima-yuta23@fixer.cd.jp>

---
+++ terraform/container_registry/main.tf
@@ -0,0 +1,7 @@
+module "container_registry" {
+  source              = "git::https://github.com/nakashima-yuta23-fixer/TerraformCommonModules.git//container_registry?ref=main"
+  product_name        = "nytes1"
+  env                 = "dv"
+  resource_group_name = var.resource_group_name
+  location            = "japaneast"
+}
